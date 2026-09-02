2d425f5805d1630dd810d6f3f2ef6a0e7e732283
Erik Hall <erikhall1811@gmail.com>
2026-09-02 14:45:46 +0200

    Added; Glad dependency for OpenGl Interop

---
+++ src/device/interops/magik_interop_opengl.cu
@@ -0,0 +1,6 @@
+#include "magik_interop_opengl.cuh"
+
+namespace magik::interops
+{
+
+}
---
+++ src/device/interops/magik_interop_opengl.cuh
@@ -0,0 +1,9 @@
+#pragma once
+#include <glad/glad.h>
+#include "cuda_runtime.h"
+#include "cuda_gl_interop.h"
+
+namespace magik::interops
+{
+    
+}
